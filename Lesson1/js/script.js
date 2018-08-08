1c2f4b2744ceb0fbefb85467cf791229d8cd2b37
Alexandr <norski874@gmail.com>
2018-08-08 14:14:58 +0300

    это второй комит

--- Lesson1/js/script.js
+++ Lesson1/js/script.js
@@ -1,1 +1,25 @@
-var budget = +prompt(Dfi, default?: DOMString)
+var budget = +prompt("Ваш бюджет за месяц?");
+var nameShop = prompt("Название вашего магазина?");
+
+
+var goods1 = prompt("Какой тип товаров будем продавать?");
+var goods2 = prompt("Какой тип товаров будем продавать?");
+var goods3 = prompt("Какой тип товаров будем продавать?");
+
+
+var shopGoods = [goods1, goods2, goods3];
+
+var mainList = {
+		budget,
+		nameShop,
+		shopGoods,
+		employers : {},
+		open : true
+};
+
+
+
+alert('Бюджет за один день ' + budget / 30);
+
+
+console.log(mainList);
