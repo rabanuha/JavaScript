2e7bfe1d7b26d181d105f9de491dd1dfa61f03c6
Alexandr <norski874@gmail.com>
2018-09-09 16:29:42 +0300

    Tab decoration_slider

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
 			let tabGlazing 							= document.getElementsByClassName('tab'),
 							tabContent = document.getElementsByClassName('tabcontent'),
-							
 							tabGlazingContent = document.getElementsByClassName('glazing_slider')[0];
 
 
@@ -21,6 +20,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
 			hideTabContent(1);
 
+
+
+
 			function showTabContent(b) {
 			  if (tabContent[b].classList.contains('hide')) {
 			    hideTabContent(0);
@@ -30,7 +32,7 @@ window.addEventListener('DOMContentLoaded', function() {
 			  }
 			}
 
-			
+
 			tabGlazingContent.addEventListener('click', function(event) {
 
 				let target = event.target;
@@ -45,6 +47,53 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 			});
+
+
+			// Tab internal_link
+
+			let tabDecor = document.getElementsByClassName('no_click'),
+							a = document.getElementsByClassName('text'),
+							tabContentDecor = document.getElementsByClassName('decorat'),
+							tabDecorSlider = document.getElementsByClassName('decoration_slider')[0];
+
+
+				function hideTabContentDec(a) {
+				  for (let i = a; i < tabContentDecor.length; i++) {
+								tabDecor[i].classList.remove('after_click');
+				    tabContentDecor[i].classList.remove('show');
+				    tabContentDecor[i].classList.add('hide');
+				  }
+				}
+
+				hideTabContentDec(1);
+
+
+
+
+				function showTabContentDec(b) {
+				  if (tabContentDecor[b].classList.contains('hide')) {
+				    hideTabContentDec(0);
+				    tabContentDecor[b].classList.remove('hide');
+				    tabContentDecor[b].classList.add('show');
+				    tabDecor[b].classList.add('after_click');
+				  }
+				}
+
+
+				tabDecorSlider.addEventListener('click', function(event) {
+
+					let target = event.target;
+					if (target.classList.contains('no_click') || target.classList.contains('text')) {
+					  for (let i = 0; i < tabDecor.length; i++) {
+					    if (target == tabDecor[i] || target == a[i]) {
+					      showTabContentDec(i);
+					      break;
+					    }
+					  }
+					}
+
+
+				});
 
 
 }); 
