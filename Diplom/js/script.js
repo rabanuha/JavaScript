4dd4e573a1aa5bb42f6ba6131cf099d5e0cbc5e0
Alexandr <norski874@gmail.com>
2018-09-10 15:38:03 +0300

    Timer

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -96,4 +96,75 @@ window.addEventListener('DOMContentLoaded', function() {
 				});
 
 
+				// Timer
+
+				let deadLine = '2018-09-13';
+
+				function getTimeRemaining(endtime) {
+					let t = Date.parse(endtime) - Date.parse(new Date()),
+
+									seconds = Math.floor( (t/1000) % 60 ),
+									minutes = Math.floor( (t/1000/60) % 60 ),
+									hours = Math.floor( (t/(1000*60*60)) % 24 ),
+									days = Math.floor( (t/(1000*60*60*24)) );
+
+
+									return {
+										'total': t,
+										'days': days,
+										'hours': hours,
+										'minutes': minutes,
+										'seconds': seconds
+									};
+				}
+
+				function setClock(id, endtime) {
+
+					let timer = document.getElementsByClassName(id)[0],
+									days = timer.querySelector('.days'),
+									hours = timer.querySelector('.hours'),
+									minutes = timer.querySelector('.minutes'),
+									seconds = timer.querySelector('.seconds');
+
+									function updateClock() {
+										  let t = getTimeRemaining(endtime);
+										  console.log(t.days);
+										  if ( String(t.days).length < 2) {
+										  	days.innerHTML = '0' + t.days;
+										  } else {
+										  	days.innerHTML = t.days;
+										  }
+
+										  if ( String(t.hours).length < 2) {
+										  	hours.innerHTML = '0' + t.hours;
+										  } else {
+										  	hours.innerHTML = t.hours;
+										  }
+
+										  if ( String(t.minutes).length < 2) {
+										  	minutes.innerHTML = '0' + t.minutes;
+										  } else {
+										  	minutes.innerHTML = t.minutes;
+										  }
+										  if ( String(t.seconds).length < 2) {
+										  	seconds.innerHTML = '0' + t.seconds;
+										  } else {
+										  	seconds.innerHTML = t.seconds;
+										  }
+						
+										  if (t.total < 0) {
+										  		days.innerHTML = '00';
+										  		hours.innerHTML = '00';
+										  		minutes.innerHTML = '00';
+										  		seconds.innerHTML = '00';
+										  }
+			
+									};
+									updateClock();
+									setInterval(updateClock, 1000);
+				};
+
+				setClock('eTimer', deadLine);
+
+
 }); 
